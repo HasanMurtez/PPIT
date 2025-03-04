f04243039abc2779d2233c4890bf88e065d6fef9
Hasan Murtaza <g00419888@atu.ie>
2025-03-04 17:05:01 +0000

    added styling to navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Logout from "./components/Logout";
 import "./App.css";
 
+const Navbar = () => {
+  const location = useLocation();
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className={location.pathname === "/login" ? "active" : ""}>
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/signup" className={location.pathname === "/signup" ? "active" : ""}>
+            Sign Up
+          </Link>
+        </li>
+        <li>
+          <Link to="/logout" className={location.pathname === "/logout" ? "active" : ""}>
+            Logout
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
