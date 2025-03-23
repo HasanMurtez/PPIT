c79018fc48c53f92c1e854531d462266debe5194
Hasan Murtaza <g00419888@atu.ie>
2025-03-23 14:53:53 +0000

    Set up server.js Mongodb connection and CarAd model

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,4 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 4000;
 
+const cors = require('cors');
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://user1:user1@cluster0.vrhww.mongodb.net/');
+
+const carAdSchema = new mongoose.Schema({
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  price: { type: Number, required: true },
+  mileage: { type: Number },
+  description: { type: String },
+  image: { type: String },
+  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const CarAd = mongoose.model('CarAd', carAdSchema);
 
 
 
