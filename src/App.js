c2236d057dd7a291a0e51f5ba46d31df8f0d5155
Hasan Murtaza <g00419888@atu.ie>
2025-04-17 13:43:35 +0000

    created about page component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Logout from "./components/Logout";
-import PostAd from "./components/PostAd";
+import PostAd from "./components/PostAd"; 
 import ViewAds from './components/ViewAds';
 import AdDetails from "./components/AdDetails";
-import Messages from './components/Messages';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase';
-import MessageNotification from "./components/MessageNotification";
-import './App.css';
+import "./App.css";
 
 const Navbar = () => {
   const location = useLocation();
-  const [user] = useAuthState(auth);
 
   return (
     <nav>
@@ -25,42 +20,29 @@ const Navbar = () => {
             Home
           </Link>
         </li>
-        {!user ? (
-          <>
-            <li>
-              <Link to="/login" className={location.pathname === "/login" ? "active" : ""}>
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link to="/signup" className={location.pathname === "/signup" ? "active" : ""}>
-                Sign Up
-              </Link>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link to="/messages" className={location.pathname === "/messages" ? "active" : ""}>
-                Messages <MessageNotification />
-              </Link>
-            </li>
-            <li>
-              <Link to="/logout" className={location.pathname === "/logout" ? "active" : ""}>
-                Logout
-              </Link>
-            </li>
-          </>
-        )}
+        <li>
+          <Link to="/login" className={location.pathname === "/login" ? "active" : ""}>
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/signup" className={location.pathname === "/signup" ? "active" : ""}>
+            Sign Up
+          </Link>
+        </li>
+        <li>
+          <Link to="/logout" className={location.pathname === "/logout" ? "active" : ""}>
+            Logout
+          </Link>
+        </li>
         <li>
           <Link to="/post-ad" className={location.pathname === "/post-ad" ? "active" : ""}>
             Post Ad
           </Link>
-        </li>
-        <li>
           <Link to="/view-ads" className={location.pathname === "/view-ads" ? "active" : ""}>
-            View Ads
+            View ads
           </Link>
+         
         </li>
       </ul>
     </nav>
@@ -79,7 +61,6 @@ function App() {
         <Route path="/post-ad" element={<PostAd />} />
         <Route path="/view-ads" element={<ViewAds />} />
         <Route path="/ad/:id" element={<AdDetails />} />
-        <Route path="/messages" element={<Messages />} />
       </Routes>
     </Router>
   );
